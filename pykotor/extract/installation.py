th3w1zard1/PyKotor

commit 6147ac995f163a0302db9a4eff514d9cb544b8be

Refactor/cleanup installation and config.py
also fix !ReplaceFile in `[SSFList]`

pykotor/extract/installation.py
@@ -269,99 +269,13 @@ class Installation:
 
     # region Load Data
 
-    def load_chitin(self) -> None:
-        """Reloads the list of resources in the Chitin linked to the Installation."""
-        if not self._path.joinpath("chitin.key").exists():
-            self.log.add_warning(f"The chitin.key file did not exist at '{self._path!s}' when loading the installation, skipping...")
-            return
-        chitin = Chitin(self._path)
-        self._chitin = list(chitin)
-
-    def load_rims(
-        self,
-    ) -> None:
-        """Reloads the list of module files in the rims folder linked to the Installation."""
-        self._rims = self.load_resources(  # type: ignore[always a dict]
-            self.rims_path(),
-            capsule_check=is_rim_file,
-            recurse=False,
-        )
-
-    def load_modules(self) -> None:
-        """Reloads the list of modules files in the modules folder linked to the Installation."""
-        self._modules = self.load_resources(  # type: ignore[always a dict]
-            self.module_path(),
-            capsule_check=is_capsule_file,
-            recurse=False,
-        )
-    def reload_module(self, module: str) -> None:
-        """Reloads the list of resources in specified module in the modules folder linked to the Installation.
-
-        Args:
-        ----
-            module: The filename of the module.
-        """
-        self._modules[module] = list(Capsule(self.module_path() / module))
-
-    def load_lips(
-        self,
-    ) -> None:
-        """Reloads the list of modules in the lips folder linked to the Installation."""
-        self._lips = self.load_resources(  # type: ignore[always a dict]
-            self.lips_path(),
-            capsule_check=is_mod_file,
-            recurse=False,
-        )
-
-    def load_textures(
-        self,
-    ) -> None:
-        """Reloads the list of modules files in the texturepacks folder linked to the Installation."""
-        self._texturepacks = self.load_resources(  # type: ignore[always a dict]
-            self.texturepacks_path(),
-            capsule_check=is_erf_file,
-            recurse=False,
-        )
-
-
-    def load_override(self, directory: str | None = None) -> None:
-        """Loads the list of resources in a specific subdirectory of the override folder linked to the Installation.
-        If a directory argument is not passed, this will reload all subdirectories in the Override folder.
-        If directory argument is ".", this will load all the loose files in the Override folder.
-
-        the _override dict follows the following example format:
-        _override["."] = list[FileResource]  # the loose files in the Override folder
-        _override["subdir1"] = list[FileResource]  # the loose files in subdir1
-        _override["subdir2"] = list[FileResource]  # the loose files in subdir2
-        _override["subdir1/subdir3"] = list[FileResource]  # the loose files in subdir1/subdir3. Key is always a posix path (delimited by /'s)
-
-        Args:
-        ----
-            directory: The subdirectory in the override folder.
-        """
-        override_path = self.override_path()
-        if directory is not None:
-            target_dirs = [override_path / directory]
-            self._override[directory] = []
-        else:
-            target_dirs = [f for f in override_path.safe_rglob("*") if f.safe_isdir()]
-            target_dirs.append(override_path)
-            self._override = {}
-
-        for folder in target_dirs:
-            relative_folder = folder.relative_to(override_path).as_posix()  # '.' if folder is the same as override_path
-            self._override[relative_folder] = self.load_resources(
-                folder,
-                recurse=False,  # we already are recursing here in load_override
-            )
-
-    def load_resources(self, path: os.PathLike | str, capsule_check=None, recurse=True) -> dict[str, list[FileResource]] | list[FileResource]:
+    def load_resources(self, path: os.PathLike | str, capsule_check=None, recurse=False) -> dict[str, list[FileResource]] | list[FileResource]:
         """Load resources for a given path and store them in the provided list.
 
         Args:
         ----
             path_method (os.PathLike | str): path for lookup.
-            recurse (bool): whether to recurse into subfolders (default is True)
+            recurse (bool): whether to recurse into subfolders (default is False)
 
         Returns:
         -------
@@ -377,46 +291,99 @@ class Installation:
             return resources
 
         files_list: list[CaseAwarePath] = list(c_path.safe_rglob("*")) if recurse else list(c_path.safe_iterdir())
-
         for file in files_list:
             if capsule_check and capsule_check(file.name):
                 resources[file.name] = list(Capsule(file))  # type: ignore[always a dict]
-                continue
-
-            with suppress(Exception):
-                identifier = ResourceIdentifier.from_path(file)
-                resource = FileResource(
-                    identifier.resname,
-                    identifier.restype,
-                    file.stat().st_size,
-                    0,
-                    file,
-                )
-                resources.append(resource)  # type: ignore[always a list]
+            else:
+                with suppress(Exception):
+                    resource = FileResource(
+                        *ResourceIdentifier.from_path(file),
+                        file.stat().st_size,
+                        0,
+                        file,
+                    )
+                    resources.append(resource)  # type: ignore[always a list]
         if not resources or not files_list:
             self.log.add_warning(f"No resources found at '{c_path!s}' when loading the installation, skipping...")
         return resources
 
+    def load_chitin(self) -> None:
+        """Reloads the list of resources in the Chitin linked to the Installation."""
+        if not self._path.joinpath("chitin.key").exists():
+            self.log.add_warning(f"The chitin.key file did not exist at '{self._path!s}' when loading the installation, skipping...")
+            return
+        self._chitin = list(Chitin(kotor_path=self._path))
+
+    def load_lips(
+        self,
+    ) -> None:
+        """Reloads the list of modules in the lips folder linked to the Installation."""
+        self._lips = self.load_resources(self.lips_path(), capsule_check=is_mod_file)  # type: ignore[always a dict]
+
+    def load_modules(self) -> None:
+        """Reloads the list of modules files in the modules folder linked to the Installation."""
+        self._modules = self.load_resources(self.module_path(), capsule_check=is_capsule_file)  # type: ignore[always a dict]
+    def reload_module(self, module: str) -> None:
+        """Reloads the list of resources in specified module in the modules folder linked to the Installation.
+
+        Args:
+        ----
+            module: The filename of the module.
+        """
+        self._modules[module] = list(Capsule(self.module_path() / module))
+
+    def load_rims(
+        self,
+    ) -> None:
+        """Reloads the list of module files in the rims folder linked to the Installation."""
+        self._rims = self.load_resources(self.rims_path(), capsule_check=is_rim_file)  # type: ignore[always a dict]
+
+    def load_textures(
+        self,
+    ) -> None:
+        """Reloads the list of modules files in the texturepacks folder linked to the Installation."""
+        self._texturepacks = self.load_resources(self.texturepacks_path(), capsule_check=is_erf_file)  # type: ignore[always a dict]
+
+
+    def load_override(self, directory: str | None = None) -> None:
+        """Loads the list of resources in a specific subdirectory of the override folder linked to the Installation.
+        If a directory argument is not passed, this will reload all subdirectories in the Override folder.
+        If directory argument is ".", this will load all the loose files in the Override folder.
+
+        the _override dict follows the following example format:
+        _override["."] = list[FileResource]  # the loose files in the Override folder
+        _override["subdir1"] = list[FileResource]  # the loose files in subdir1
+        _override["subdir2"] = list[FileResource]  # the loose files in subdir2
+        _override["subdir1/subdir3"] = list[FileResource]  # the loose files in subdir1/subdir3. Key is always a posix path (delimited by /'s)
+
+        Args:
+        ----
+            directory: The relative path of a subfolder to the override folder.
+        """
+        override_path = self.override_path()
+        if directory:
+            target_dirs = [override_path / directory]
+            self._override[directory] = []
+        else:
+            target_dirs = [f for f in override_path.safe_rglob("*") if f.safe_isdir()]
+            target_dirs.append(override_path)
+            self._override = {}
+
+        for folder in target_dirs:
+            relative_folder = folder.relative_to(override_path).as_posix()  # '.' if folder is the same as override_path
+            self._override[relative_folder] = self.load_resources(folder)  # type: ignore[always a list]
+
     def load_streammusic(self) -> None:
         """Reloads the list of resources in the streammusic folder linked to the Installation."""
-        self._streammusic = self.load_resources(  # type: ignore[always a list]
-            self.streammusic_path(),
-            recurse=False,
-        )
+        self._streammusic = self.load_resources(self.streammusic_path())  # type: ignore[always a list]
 
     def load_streamsounds(self) -> None:
         """Reloads the list of resources in the streamsounds folder linked to the Installation."""
-        self._streamsounds = self.load_resources(  # type: ignore[always a list]
-            self.streamsounds_path(),
-            recurse=False,
-        )
+        self._streamsounds = self.load_resources(self.streamsounds_path())  # type: ignore[always a list]
 
     def load_streamwaves(self) -> None:
         """Reloads the list of resources in the streamvoice/streamwaves folder linked to the Installation."""
-        self._streamwaves = self.load_resources(  # type: ignore[always a list]
-            self.streamwaves_path(),
-            recurse=True,
-        )
+        self._streamwaves = self.load_resources(self.streamwaves_path(), recurse=True)  # type: ignore[always a list]
 
     # endregion
 
@@ -602,8 +569,13 @@ class Installation:
             capsules=capsules,
             folders=folders,
         )
+        search = batch[query]
+        if not search or not search.data:
+            self.log.add_error(
+                f"Could not find '{resname}.{restype}' during resource lookup.",
+            )
 
-        return batch[query] or None
+        return None
 
     def resources(
         self,

pykotor/tools/misc.py
@@ -2,13 +2,17 @@ from __future__ import annotations
 
 import hashlib
 import re
+from typing import TYPE_CHECKING
 
-from pykotor.tools.path import CaseAwarePath
+from pykotor.tools.path import Path
+
+if TYPE_CHECKING:
+    import os
 
 
-def generate_filehash_sha256(filepath: str | CaseAwarePath) -> str:
+def generate_filehash_sha256(filepath: os.PathLike | str) -> str:
     sha1_hash = hashlib.sha256()
-    filepath = filepath if isinstance(filepath, CaseAwarePath) else CaseAwarePath(filepath)
+    filepath = filepath if isinstance(filepath, Path) else Path(filepath)
     with filepath.open("rb") as f:
         data = f.read(65536)
         while data:  # read in 64k chunks
@@ -95,7 +99,7 @@ def case_insensitive_replace(s: str, old: str, new: str) -> str:
 def striprtf(text):
     """Strips RTF encoding utterly and completely."""
     pattern = re.compile(r"\\([a-z]{1,32})(-?\d{1,10})?[ ]?|\\'([0-9a-f]{2})|\\([^a-z])|([{}])|[\r\n]+|(.)", re.I)
-    # control words which specify a "destionation".
+    # control words which specify a "destination".
     destinations = frozenset(
         (
             "aftncn",

pykotor/tslpatcher/config.py
@@ -19,7 +19,6 @@ from pykotor.extract.file import ResourceIdentifier
 from pykotor.extract.installation import (
     Installation,
     SearchLocation,
-    is_kotor_install_dir,
 )
 from pykotor.resource.formats.erf import ERF, read_erf, write_erf
 from pykotor.resource.formats.erf.erf_data import ERFType
@@ -198,24 +197,9 @@ class ModInstaller:
         msg = "Could not determine which KOTOR game to load!"
         raise ValueError(msg)
 
-    # extract into multiple funcs perhaps?
-    def install(self) -> None:
-        config = self.config()
-        if config.required_file:
-            requiredfile_path = self.game_path / "Override" / config.required_file
-            if not requiredfile_path.exists():
-                raise ImportError(config.required_message.strip() or "cannot install - missing a required mod")
-
-        installation = Installation(self.game_path, self.log)
-        memory = PatcherMemory()
-        twodas = {}
-        soundsets = {}
-        templates = {}
-
-        # Create a timestamped backup directory
-        tz_aware_datetime = datetime.now(tz=timezone.utc).astimezone()
-        timestamp = tz_aware_datetime.strftime("%Y-%m-%d_%H.%M.%S")
+    def initialize_backup(self) -> CaseAwarePath:
         backup_dir = self.mod_path
+        timestamp = datetime.now(tz=timezone.utc).astimezone().strftime("%Y-%m-%d_%H.%M.%S")
         while not backup_dir.joinpath("tslpatchdata").exists() and backup_dir.parent.name:
             backup_dir = backup_dir.parent
         uninstall_dir = backup_dir.joinpath("uninstall")
@@ -230,23 +214,34 @@ class ModInstaller:
         except PermissionError as e:
             self.log.add_warning(f"Could not create backup folder: {e}")
         self.log.add_note(f"Using backup directory: '{backup_dir}'")
+        return backup_dir
 
+    def install(self) -> None:
+        config = self.config()
+        if config.required_file:
+            requiredfile_path = self.game_path / "Override" / config.required_file
+            if not requiredfile_path.exists():
+                raise ImportError(config.required_message.strip() or "cannot install - missing a required mod")
+
+        installation = Installation(self.game_path, self.log)
+        memory = PatcherMemory()
+
+        # Create a timestamped backup directory
+        backup_dir = self.initialize_backup()
         processed_files = set()
 
         self.log.add_note(f"Applying {len(config.patches_tlk.modifiers)} patches from [TLKList]...")
         if len(config.patches_tlk.modifiers) > 0:  # skip if no patches need to be made (faster)
-            dialog_tlk_path = (
-                self.game_path / "dialog.tlk"
-            )  # sourcery skip: extract-method, move-assign-in-block, use-fstring-for-concatenation
-            dialog_tlk = read_tlk(dialog_tlk_path)
+            # sourcery skip: extract-method, move-assign-in-block, use-fstring-for-concatenation
+            dialog_tlk_path = self.game_path.joinpath("dialog.tlk")
+
             create_backup(self.log, dialog_tlk_path, backup_dir, processed_files)
-            self.log.add_note("Patching dialog.tlk...")
+
+            self.log.add_note("Patching 'dialog.tlk'...")
+            dialog_tlk = read_tlk(dialog_tlk_path)
+
             config.patches_tlk.apply(dialog_tlk, memory)
-            write_tlk(
-                dialog_tlk,
-                dialog_tlk_path,
-                strip_soundlength=self.game() == Game.K2,
-            )
+            write_tlk(dialog_tlk, dialog_tlk_path, strip_soundlength=self.game() == Game.K2)
             self.log.complete_patch()
 
         # Move nwscript.nss to Override if there are any nss patches to do
@@ -265,197 +260,101 @@ class ModInstaller:
         self.log.add_note(f"Applying {len(config.patches_2da)} patches from [2DAList]...")
         for twoda_patch in config.patches_2da:
             twoda_output_filepath = self.game_path / "Override" / twoda_patch.filename
-            resname, restype = ResourceIdentifier.from_path(twoda_patch.filename)
 
+            create_backup(self.log, twoda_output_filepath, backup_dir, processed_files, subdirectory_path="Override")
+
+            self.log.add_note(f"Patching '{twoda_patch.filename}' in the 'Override' folder.")
             search = installation.resource(
-                resname,
-                restype,
+                *ResourceIdentifier.from_path(twoda_patch.filename),
                 [SearchLocation.OVERRIDE, SearchLocation.CUSTOM_FOLDERS],
                 folders=[self.mod_path],
             )
-            if search is None or search.data is None:
-                self.log.add_error(
-                    f"Didn't patch '{twoda_patch.filename}' because search data is `None`.",
-                )
+            if search is None:
                 continue
-
             twoda: TwoDA = read_2da(search.data)
-            twodas[twoda_patch.filename] = twoda
 
-            create_backup(
-                self.log,
-                twoda_output_filepath,
-                backup_dir,
-                processed_files,
-                subdirectory_path="Override",
-            )
-            self.log.add_note(f"Patching '{twoda_patch.filename}'")
             twoda_patch.apply(twoda, memory)
-
             write_2da(twoda, twoda_output_filepath)
-
             self.log.complete_patch()
 
         self.log.add_note(f"Applying {len(config.patches_gff)} patches from [GFFList]...")
         for gff_patch in config.patches_gff:
             gff_output_container_path = self.game_path / gff_patch.destination
             rel_output_container_path = gff_output_container_path.relative_to(self.game_path)
+            capsule: Capsule | None = None
 
-            capsule = None
             if is_capsule_file(gff_patch.destination.name):
                 capsule = Capsule(gff_output_container_path)
-
-            # TSLPatcher follows the following behavior:
-            # if !ReplaceFile=0:
-            # 1. Get the resource from the Modules (if we're patching to one).
-            # 2. Get the resource from Override (if we're patching there).
-            # 3. If still not found, get it from tslpatchdata
-            # if !ReplaceFile=1:
-            # 1. Get the resource from tslpatchdata.
-            # 2. If not found, get the resource from the Module we're patching (if we're patching to one).
-            # 3. If still not found, get the resource from Override (if we're patching there)
-            game_resource_location = SearchLocation.OVERRIDE if capsule is None else SearchLocation.CUSTOM_MODULES
-            if gff_patch.replace_file:
-                search_order = [
-                    SearchLocation.CUSTOM_FOLDERS,
-                    game_resource_location,
-                ]
-            else:
-                search_order = [
-                    game_resource_location,
-                    SearchLocation.CUSTOM_FOLDERS,
-                ]
-
-            resname, restype = ResourceIdentifier.from_path(gff_patch.filename)
-            search = installation.resource(
-                resname,
-                restype,
-                search_order,
-                folders=[self.mod_path],
-                capsules=[] if capsule is None else [capsule],
-            )
-            if search is None or search.data is None:
-                self.log.add_error(
-                    f"Didn't patch '{gff_patch.filename}' because search data is `None`.",
-                )
-                continue
-
-            if capsule is not None:
                 create_backup(self.log, gff_output_container_path, backup_dir, processed_files, rel_output_container_path.parent)
-                self.log.add_note(
-                    f"Patching '{gff_patch.filename}' in the '{rel_output_container_path}' archive.",
-                )
                 if not capsule._path.exists():
                     self.log.add_error(
                         f"The capsule '{rel_output_container_path}' did not exist when patching GFF '{gff_patch.filename}'."
                         " This most likely indicates a different problem existed beforehand, such as a missing mod dependency.",
                     )
             else:
-                create_backup(
-                    self.log,
-                    gff_output_container_path / gff_patch.filename,
-                    backup_dir,
-                    processed_files,
-                    rel_output_container_path,
-                )
-                self.log.add_note(
-                    f"Patching '{gff_patch.filename}' in the '{rel_output_container_path}' folder.",
-                )
+                create_backup(self.log, gff_output_container_path.joinpath(gff_patch.filename), backup_dir, processed_files, rel_output_container_path)
 
-            template = templates[gff_patch.filename] = read_gff(search.data)
+            self.log.add_note(f"Patching '{gff_patch.filename}' in the '{rel_output_container_path}' folder.")
+            search = installation.resource(
+                *ResourceIdentifier.from_path(gff_patch.filename),
+                [SearchLocation.CUSTOM_FOLDERS] if gff_patch.replace_file else [SearchLocation.CUSTOM_MODULES, SearchLocation.OVERRIDE, SearchLocation.CUSTOM_FOLDERS],
+                capsules=[capsule] if capsule else [],
+                folders=[self.mod_path],
+            )
+            if not search:
+                continue
+            template = read_gff(search.data)
 
             gff_patch.apply(template, memory, self.log)
-            self.write(
-                gff_output_container_path,
-                gff_patch.filename,
-                bytes_gff(template),
-                replace=True,
-            )
-
+            self.write(gff_output_container_path, gff_patch.filename, bytes_gff(template), replace=True)
             self.log.complete_patch()
 
         self.log.add_note(f"Applying {len(config.patches_nss)} patches from [CompileList]...")
         for nss_patch in config.patches_nss:
-            capsule: Capsule | None = None
             nss_output_container_path: CaseAwarePath = self.game_path / nss_patch.destination
             rel_output_container_path: CaseAwarePath = nss_output_container_path.relative_to(self.game_path)
             ncs_compiled_filename = f"{nss_patch.filename.rsplit('.', 1)[0]}.ncs"
 
             if is_capsule_file(nss_output_container_path.name):
-                capsule = Capsule(nss_output_container_path)
-                create_backup(
-                    self.log,
-                    nss_output_container_path,
-                    backup_dir,
-                    processed_files,
-                    rel_output_container_path.parent,
-                )
+                create_backup(self.log, nss_output_container_path, backup_dir, processed_files, rel_output_container_path.parent)
                 if not nss_output_container_path.exists():
                     self.log.add_error(
                         f"The capsule '{rel_output_container_path}' did not exist when compiling NSS '{nss_patch.filename}'."
                         " This most likely indicates a different problem existed beforehand, such as a missing mod dependency.",
                     )
             else:
-                create_backup(
-                    self.log,
-                    nss_output_container_path / ncs_compiled_filename,
-                    backup_dir,
-                    processed_files,
-                    rel_output_container_path,
-                )
+                create_backup(self.log, nss_output_container_path.joinpath(nss_patch.filename), backup_dir, processed_files, rel_output_container_path)
 
-            self.log.add_note(
-                f"Compiling '{nss_patch.filename}' and saving to '{rel_output_container_path / ncs_compiled_filename}'",
-            )
-
+            self.log.add_note(f"Compiling '{nss_patch.filename}' and saving to '{rel_output_container_path / ncs_compiled_filename}'")
             nss_bytes = BinaryReader.load_file(self.mod_path / nss_patch.filename)
             encoding: str = (chardet and chardet.detect(nss_bytes) or {}).get("encoding") or "utf8"
             nss: list[str] = [nss_bytes.decode(encoding=encoding, errors="replace")]
 
             nss_patch.apply(nss, memory, self.log)
-
-            self.write(
-                nss_output_container_path,
-                ncs_compiled_filename,
-                bytes_ncs(compile_nss(nss[0], installation.game())),
-                nss_patch.replace_file,
-            )
-
+            self.write(nss_output_container_path, ncs_compiled_filename, bytes_ncs(compile_nss(nss[0], installation.game())), nss_patch.replace_file)
             self.log.complete_patch()
 
         self.log.add_note(f"Applying {len(config.patches_ssf)} patches from [SSFList]...")
         for ssf_patch in config.patches_ssf:
             ssf_output_filepath: CaseAwarePath = self.game_path / "Override" / ssf_patch.filename
 
-            resname, restype = ResourceIdentifier.from_path(ssf_patch.filename)
+            create_backup(self.log, ssf_output_filepath, backup_dir, processed_files, "Override")
+
+            self.log.add_note(f"Patching '{ssf_patch.filename}' in the 'Override' folder.")
             search = installation.resource(
-                resname,
-                restype,
-                [SearchLocation.OVERRIDE, SearchLocation.CUSTOM_FOLDERS],
+                *ResourceIdentifier.from_path(ssf_patch.filename),
+                [SearchLocation.CUSTOM_FOLDERS] if ssf_patch.replace_file else [SearchLocation.OVERRIDE, SearchLocation.CUSTOM_FOLDERS],
                 folders=[self.mod_path],
             )
-            if search is None or search.data is None:
-                self.log.add_error(
-                    f"Didn't patch '{ssf_patch.filename}' because search data is `None`.",
-                )
+            if search is None:
                 continue
+            soundset = read_ssf(search.data)
 
-            soundset = soundsets[ssf_patch.filename] = read_ssf(search.data)
-
-            create_backup(self.log, ssf_output_filepath, backup_dir, processed_files, "Override")
-            self.log.add_note(f"Patching '{ssf_patch.filename}' in the 'Override' folder.")
             ssf_patch.apply(soundset, memory)
             write_ssf(soundset, ssf_output_filepath)
-
             self.log.complete_patch()
 
-    def write(
-        self,
-        destination: CaseAwarePath,
-        filename: str,
-        data: bytes,
-        replace: bool = False,
-    ) -> None:
+    def write(self, destination: CaseAwarePath, filename: str, data: bytes, replace: bool = False) -> None:
         resname, restype = ResourceIdentifier.from_path(filename)
         if is_rim_file(destination.name):
             rim = read_rim(BinaryReader.load_file(destination)) if destination.exists() else RIM()

commit fdfe5f31fc465e19a5459744bcd09f3eb587d385

fix bug caused by commit 6147ac99

pykotor/extract/installation.py
@@ -574,8 +574,8 @@ class Installation:
             self.log.add_error(
                 f"Could not find '{resname}.{restype}' during resource lookup.",
             )
-
-        return None
+            return None
+        return search
 
     def resources(
         self,