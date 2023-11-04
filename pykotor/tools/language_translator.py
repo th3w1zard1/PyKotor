b633f970cd2e3d28f20f49ef5a349e453ed567c6
Benjamin Auquite <halomastar@gmail.com>
2023-11-04 16:18:26 -0500

    translate in chunks of 500 characters.

--- pykotor/tools/language_translator.py
+++ pykotor/tools/language_translator.py
@@ -43,25 +43,59 @@ class Translator:
     def translate(self, text: str, from_lang: Language | None = None, to_lang: Language | None = None) -> str:
         if not self._initialized:
             self.initialize()
-        translated_text = text
+        translated_text: str = ""
         to_lang = to_lang or self.to_lang
-        from_lang = (from_lang or self.from_lang)
+        from_lang = from_lang or self.from_lang
         from_lang_code: str = from_lang.get_language_code()  # type: ignore[union-attr]
         to_lang_code: str = to_lang.get_language_code()  # type: ignore[union-attr]
 
-        if self.translation_option == TranslationOption.GOOGLETRANS:
-            translated_text = self._translator.translate(text, src=from_lang_code, dest=to_lang_code).text  # type: ignore[attr-defined]
-        elif self.translation_option == TranslationOption.LIBRE:
-            if from_lang is None:
-                msg = "LibreTranslate requires a specified source language."
-                raise ValueError(msg)
-            translated_text = self._translator.translate(text, source=from_lang_code, target=to_lang_code)  # type: ignore[attr-defined]
-        elif self.translation_option == TranslationOption.DL_TRANSLATE:
-            translated_text = self._translator.translate(text, source=from_lang.name, target=to_lang.name)  # type: ignore[attr-defined, union-attr]
-        elif self.translation_option == TranslationOption.TRANSLATE:
-            translated_text = self._translator.translate(text)  # type: ignore[attr-defined]
-        else:
-            msg = "Invalid translation option selected"
-            raise ValueError(msg)
+        # Function to chunk the text into segments with a maximum of 500 characters
+        def chunk_text(text, size):
+            chunks = []
+            while text:
+                if len(text) <= size:
+                    chunks.append(text)
+                    break
+                # Find the last complete word or sentence end within the size limit
+                end = min(size, len(text))
+                last_space = text.rfind(" ", 0, end)
+                last_period = text.rfind(". ", 0, end)
+                cut_off = max(last_space, last_period + 1) if last_period != -1 else last_space
+                if cut_off == -1:  # In case there's a very long word
+                    cut_off = size
+                chunks.append(text[:cut_off])
+                text = text[cut_off:].lstrip()  # Remove leading whitespace from next chunk
+            return chunks
 
-        return translated_text  # type: ignore[reportGeneralTypeIssues]
+        # Break the text into 500-character chunks
+        chunks = chunk_text(text, 500)
+        for chunk in chunks:
+            # Ensure not cutting off in the middle of a word
+            if len(chunk) == 500 and not text[len(chunk)].isspace():
+                cut_off = chunk.rfind(" ")
+                next_chunk = chunk[cut_off:] + (chunks[chunks.index(chunk) + 1] if len(chunks) > chunks.index(chunk) + 1 else "")
+                chunk = chunk[:cut_off]
+                if next_chunk:
+                    chunks[chunks.index(chunk) + 1] = next_chunk
+
+            # Translate each chunk
+            partial_translation = ""
+            if self.translation_option == TranslationOption.GOOGLETRANS:
+                partial_translation = self._translator.translate(chunk, src=from_lang_code, dest=to_lang_code).text  # type: ignore[attr-defined]
+            elif self.translation_option == TranslationOption.LIBRE:
+                if from_lang is None:
+                    msg = "LibreTranslate requires a specified source language."
+                    raise ValueError(msg)
+                partial_translation = self._translator.translate(chunk, source=from_lang_code, target=to_lang_code)  # type: ignore[attr-defined]
+            elif self.translation_option == TranslationOption.DL_TRANSLATE:
+                partial_translation = self._translator.translate(chunk, source=from_lang.name, target=to_lang.name)  # type: ignore[attr-defined, union-attr]
+            elif self.translation_option == TranslationOption.TRANSLATE:
+                partial_translation = self._translator.translate(chunk)  # type: ignore[attr-defined]
+            else:
+                msg = "Invalid translation option selected"
+                raise ValueError(msg)
+
+            # Append the partial translation to the full translation
+            translated_text += partial_translation  # type: ignore[reportGeneralTypeIssues]
+
+        return translated_text
