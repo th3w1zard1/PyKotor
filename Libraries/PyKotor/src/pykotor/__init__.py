a8b8ace40a5cbcb429af6a961601d075d25ff2a2
Benjamin Auquite <halomastar@gmail.com>
2025-12-05 22:36:33 -0600

    fix: add missing __init__.py for pykotor package to resolve Pyright import errors

---
+++ Libraries/PyKotor/src/pykotor/__init__.py
@@ -0,0 +1,8 @@
+"""PyKotor library for Knights of the Old Republic game resource manipulation.
+
+This library provides tools for reading, writing, and manipulating KOTOR game resources
+including BIF, ERF, GFF, 2DA, TLK, and many other file formats.
+"""
+
+__all__: list[str] = []
+
