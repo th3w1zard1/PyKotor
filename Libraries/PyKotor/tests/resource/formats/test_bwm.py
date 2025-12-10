50d48b7b62d4b888153df6d43fd7db230a0d9fdf
Benjamin Auquite <halomastar@gmail.com>
2025-12-09 19:57:06 -0600

    fix: correct TEST_TOOLSET_WOK_FILE path to use TESTS_DIR

--- Libraries/PyKotor/tests/resource/formats/test_bwm.py
+++ Libraries/PyKotor/tests/resource/formats/test_bwm.py
@@ -50,7 +50,7 @@ from pykotor.resource.formats.bwm.bwm_data import BWM, BWMType, BWMFace  # noqa:
 # Test file paths
 TESTS_DIR = THIS_FILE.parents[2]  # Goes up to 'tests' directory
 TEST_WOK_FILE = TESTS_DIR / "test_files" / "test.wok"
-TEST_TOOLSET_WOK_FILE = REPO_ROOT / "tests" / "test_toolset" / "test_files" / "zio006j.wok"
+TEST_TOOLSET_WOK_FILE = TESTS_DIR / "test_files" / "zio006j.wok"
 
 
 class TestBWMHeaderValidation:
