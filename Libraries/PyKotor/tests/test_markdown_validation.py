5b54365c61ab02f011c99d119e162df3c98e18fc
Benjamin Auquite <halomastar@gmail.com>
2025-12-10 08:21:34 -0600

    test: update overly broad hyperlink detection to match all revert script patterns

--- Libraries/PyKotor/tests/test_markdown_validation.py
+++ Libraries/PyKotor/tests/test_markdown_validation.py
@@ -361,13 +361,6 @@ OVERLY_BROAD_PATTERNS = [
         "Common word 'pointer' should not be linked to generic GFF file-structure anchor",
     ),
     (
-        r"^(pointer|pointers)$",
-        [
-            r"GFF-File-Format#file-structure-overview",
-        ],
-        "Common word 'pointer' should not be linked to generic GFF file-structure-overview anchor",
-    ),
-    (
         r"^(header|headers)$",
         [
             r"GFF-File-Format#file-header",
@@ -388,13 +381,6 @@ OVERLY_BROAD_PATTERNS = [
             r"GFF-File-Format#gff-data-types",
         ],
         "Common word 'bit' should not be linked to generic GFF data-types anchor (unless in technical context like '32-bit')",
-    ),
-    (
-        r"^(bit|bits)$",
-        [
-            r"GFF-File-Format#gff-data-types",
-        ],
-        "Common word 'bit' should not be linked to generic GFF gff-data-types anchor (unless in technical context like '32-bit')",
     ),
     (
         r"^(mask|masks|bitmask|bitmasks)$",
