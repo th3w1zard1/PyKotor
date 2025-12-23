35e6a8bdc0de462c85372a2a145332dfaddaa5c5
Benjamin Auquite <halomastar@gmail.com>
2025-12-23 17:09:42 -0600

    refactor: inline test.are file into test_are.py

--- Libraries/PyKotor/tests/resource/generics/test_are.py
+++ Libraries/PyKotor/tests/resource/generics/test_are.py
@@ -35,7 +35,112 @@ if TYPE_CHECKING:
     from pykotor.resource.formats.gff import GFF
     from pykotor.resource.generics.are import ARE
 
-TEST_FILE = "Libraries/PyKotor/tests/test_files/test.are"
+# Inlined test.are content converted to XML format
+TEST_ARE_XML = """<gff3>
+  <struct id="-1">
+    <sint32 label="ID">0</sint32>
+    <sint32 label="Creator_ID">0</sint32>
+    <uint32 label="Version">88</uint32>
+    <exostring label="Tag">Untitled</exostring>
+    <locstring label="Name" strref="75101" />
+    <exostring label="Comments">comments</exostring>
+    <struct label="Map" id="0">
+      <sint32 label="MapResX">18</sint32>
+      <sint32 label="NorthAxis">1</sint32>
+      <float label="WorldPt1X">-14.180000305175781</float>
+      <float label="WorldPt1Y">-15.0600004196167</float>
+      <float label="WorldPt2X">13.279999732971191</float>
+      <float label="WorldPt2Y">12.859999656677246</float>
+      <float label="MapPt1X">0.3779999911785126</float>
+      <float label="MapPt1Y">0.7680000066757202</float>
+      <float label="MapPt2X">0.621999979019165</float>
+      <float label="MapPt2Y">0.27300000190734863</float>
+      <sint32 label="MapZoom">1</sint32>
+      </struct>
+    <list label="Expansion_List" />
+    <uint32 label="Flags">0</uint32>
+    <sint32 label="ModSpotCheck">0</sint32>
+    <sint32 label="ModListenCheck">0</sint32>
+    <float label="AlphaTest">0.20000000298023224</float>
+    <sint32 label="CameraStyle">1</sint32>
+    <resref label="DefaultEnvMap">defaultenvmap</resref>
+    <resref label="Grass_TexName">grasstexture</resref>
+    <float label="Grass_Density">1.0</float>
+    <float label="Grass_QuadSize">1.0</float>
+    <uint32 label="Grass_Ambient">16777215</uint32>
+    <uint32 label="Grass_Diffuse">16777215</uint32>
+    <uint32 label="Grass_Emissive">16777215</uint32>
+    <float label="Grass_Prob_LL">0.25</float>
+    <float label="Grass_Prob_LR">0.25</float>
+    <float label="Grass_Prob_UL">0.25</float>
+    <float label="Grass_Prob_UR">0.25</float>
+    <uint32 label="MoonAmbientColor">0</uint32>
+    <uint32 label="MoonDiffuseColor">0</uint32>
+    <byte label="MoonFogOn">0</byte>
+    <float label="MoonFogNear">99.0</float>
+    <float label="MoonFogFar">100.0</float>
+    <uint32 label="MoonFogColor">0</uint32>
+    <byte label="MoonShadows">0</byte>
+    <uint32 label="SunAmbientColor">16777215</uint32>
+    <uint32 label="SunDiffuseColor">16777215</uint32>
+    <byte label="SunFogOn">1</byte>
+    <float label="SunFogNear">99.0</float>
+    <float label="SunFogFar">100.0</float>
+    <uint32 label="SunFogColor">16777215</uint32>
+    <byte label="SunShadows">1</byte>
+    <uint32 label="DynAmbientColor">16777215</uint32>
+    <byte label="IsNight">0</byte>
+    <byte label="LightingScheme">0</byte>
+    <byte label="ShadowOpacity">205</byte>
+    <byte label="DayNightCycle">0</byte>
+    <sint32 label="ChanceRain">99</sint32>
+    <sint32 label="ChanceSnow">99</sint32>
+    <sint32 label="ChanceLightning">99</sint32>
+    <sint32 label="WindPower">1</sint32>
+    <uint16 label="LoadScreenID">0</uint16>
+    <byte label="PlayerVsPlayer">3</byte>
+    <byte label="NoRest">0</byte>
+    <byte label="NoHangBack">0</byte>
+    <byte label="PlayerOnly">0</byte>
+    <byte label="Unescapable">1</byte>
+    <byte label="DisableTransit">1</byte>
+    <byte label="StealthXPEnabled">1</byte>
+    <uint32 label="StealthXPLoss">25</uint32>
+    <uint32 label="StealthXPMax">25</uint32>
+    <sint32 label="DirtyARGBOne">123</sint32>
+    <sint32 label="DirtySizeOne">1</sint32>
+    <sint32 label="DirtyFormulaOne">1</sint32>
+    <sint32 label="DirtyFuncOne">1</sint32>
+    <sint32 label="DirtyARGBTwo">1234</sint32>
+    <sint32 label="DirtySizeTwo">1</sint32>
+    <sint32 label="DirtyFormulaTwo">1</sint32>
+    <sint32 label="DirtyFuncTwo">1</sint32>
+    <sint32 label="DirtyARGBThree">12345</sint32>
+    <sint32 label="DirtySizeThree">1</sint32>
+    <sint32 label="DirtyFormulaThre">1</sint32>
+    <sint32 label="DirtyFuncThree">1</sint32>
+    <list label="Rooms">
+      <struct id="0">
+        <exostring label="RoomName">002ebo</exostring>
+        <sint32 label="EnvAudio">17</sint32>
+        <float label="AmbientScale">0.9300000071525574</float>
+        <sint32 label="ForceRating">1</sint32>
+        <byte label="DisableWeather">1</byte>
+        </struct>
+      <struct id="0">
+        <exostring label="RoomName">002ebo2</exostring>
+        <sint32 label="EnvAudio">17</sint32>
+        <float label="AmbientScale">0.9800000190734863</float>
+        <sint32 label="ForceRating">2</sint32>
+        <byte label="DisableWeather">0</byte>
+        </struct>
+      </list>
+    <resref label="OnEnter">k_on_enter</resref>
+    <resref label="OnExit">onexit</resref>
+    <resref label="OnHeartbeat">onheartbeat</resref>
+    <resref label="OnUserDefined">onuserdefined</resref>
+    </struct>
+  </gff3>"""
 K1_PATH: str | None = os.environ.get("K1_PATH", "C:\\Program Files (x86)\\Steam\\steamapps\\common\\swkotor")
 K2_PATH: str | None = os.environ.get("K2_PATH", "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Knights of the Old Republic II")
 
@@ -48,12 +153,12 @@ class TestARE(unittest.TestCase):
         self.log_messages.append(message)
 
     def test_io_construct(self):
-        gff: GFF = read_gff(TEST_FILE)
+        gff: GFF = read_gff(TEST_ARE_XML.encode('utf-8'), file_format=ResourceType.GFF_XML)
         are: ARE = construct_are(gff)
         self.validate_io(are)
 
     def test_io_reconstruct(self):
-        gff = read_gff(TEST_FILE)
+        gff = read_gff(TEST_ARE_XML.encode('utf-8'), file_format=ResourceType.GFF_XML)
         gff = dismantle_are(construct_are(gff), Game.K2)
         are = construct_are(gff)
         self.validate_io(are)
